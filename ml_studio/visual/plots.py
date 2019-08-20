24b7af066f8ea8703efb12f6c63d317b84191c0d
John James <jjames@decisionscients.com>
2019-08-20 14:58:39 -0400

    Refactored basic and learning plots

--- ml_studio/visual/plots.py
+++ ml_studio/visual/plots.py
@@ -1,30 +1,32 @@
 # =========================================================================== #
 #                                PLOTS                                        #
 # =========================================================================== #
+from ml_studio.utils.filemanager import save_fig
+import seaborn as sns
+import pandas as pd
+import numpy as np
 """Class containing basic plotting functions"""
 # --------------------------------------------------------------------------- #
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-whitegrid')
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from ml_studio.utils.filemanager import save_fig
 
 # --------------------------------------------------------------------------- #
 #                            Learning Curves                                  #
 # --------------------------------------------------------------------------- #
+
+
 class LearningCurves():
 
     def __init__(self):
-            pass  
+        pass
 
-    def optimization(self, history, directory=None, filename=None, 
-                         xlim=None, ylim=None,show=True):
+    def optimization(self, history, directory=None, filename=None,
+                     xlim=None, ylim=None, figsize=(12, 4), show=True):
         """Renders optimization learning curves for training and validation"""
 
         # Extract parameters and data
-        d = {'Epoch': history.epochs,              
+        d = {'Epoch': history.epochs,
              'Training Cost': history.epoch_log.get('train_cost'),
              'Validation Cost': history.epoch_log.get('val_cost')}
         df = pd.DataFrame(data=d)
@@ -34,81 +36,42 @@ class LearningCurves():
         title = history.params.get('name') + "\n" + "Optimization Learning Curves" + \
             '\n' + proper(history.params.get('cost')) + " Cost"
 
-
-        # Initialize plot and set aesthetics        
-        fig, ax = plt.subplots(figsize=(12,4))               
-        sns.set(style="whitegrid", font_scale=1)
-        ax.set_facecolor('w')
-        ax.tick_params(colors='k')
-        ax.xaxis.label.set_color('k')
-        ax.yaxis.label.set_color('k')
-        ax.set_xlabel('Epoch')
-        ax.set_ylabel(proper(history.params.get('cost')) + "Cost")
-        ax.set_title(title, color='k')            
-
-        # Plot Cost
-        ax = sns.lineplot(x='Epoch', y='Cost', hue='Dataset', data=df, ax=ax)   
-        # Set x and y limits
-        if xlim is not None:
-            ax.set_xlim(left = xlim[0], right=xlim[1])
-        if ylim is not None:
-            ax.set_xlim(bottom=ylim[0], top=ylim[1])
-
+        # Render and save plot (if necessary)
+        plot = BasicPlots()
+        fig, _ = plot.lineplot(data=df, x=df.columns[0], y='Cost',
+                               z='Dataset', title=title, log=False, xlim=xlim, ylim=ylim,
+                               directory=directory, filename=filename, figsize=figsize)
         # Show plot
         fig.tight_layout()
         if show:
             plt.show()
-        # Save plot if instructed to do so
-        if directory is not None:
-            if filename is None:
-                filename = title.strip('\n') + '.png'
-            save_fig(fig, directory, filename)       
-        return fig     
+        return fig
 
-    def performance(self, history, directory=None, filename=None, 
-                         xlim=None, ylim=None,show=True):
+    def performance(self, history, directory=None, filename=None,
+                    xlim=None, ylim=None, figsize=(12, 4), show=True):
         """Renders performance learning curves for training and validation"""
 
         # Extract parameters and data
-        d = {'Epoch': history.epochs,              
+        d = {'Epoch': history.epochs,
              'Training Scores': history.epoch_log.get('train_score'),
              'Validation Scores': history.epoch_log.get('val_score')}
         df = pd.DataFrame(data=d)
         df = pd.melt(df, id_vars='Epoch', value_vars=['Training Scores', 'Validation Scores'],
-                     var_name=['Dataset'], value_name='Scores')
+                     var_name=['Dataset'], value_name=proper(history.params.get('metric')))
         # Format title
         title = history.params.get('name') + "\n" + "Performance Learning Curves" + \
-            '\n' + proper(history.params.get('metric')) 
+            '\n' + proper(history.params.get('metric')) + "Score"
 
-        # Initialize plot and set aesthetics        
-        fig, ax = plt.subplots(figsize=(12,4))               
-        sns.set(style="whitegrid", font_scale=1)
-        ax.set_facecolor('w')
-        ax.tick_params(colors='k')
-        ax.xaxis.label.set_color('k')
-        ax.yaxis.label.set_color('k')
-        ax.set_xlabel('Epoch')
-        ax.set_ylabel(proper(history.params.get('metric')) + "Score")
-        ax.set_title(title, color='k')            
-
-        # Plot Scores
-        ax = sns.lineplot(x='Epoch', y='Scores', hue='Dataset', data=df, ax=ax)   
-        # Set x and y limits
-        if xlim is not None:
-            ax.set_xlim(left = xlim[0], right=xlim[1])
-        if ylim is not None:
-            ax.set_xlim(bottom=ylim[0], top=ylim[1])
-
+        # Render and save plot (if necessary)
+        plot = BasicPlots()
+        fig, _ = plot.lineplot(data=df, x=df.columns[0], y=history.params.get('metric'),
+                               z='Dataset', title=title, log=False, xlim=xlim, ylim=ylim,
+                               directory=directory, filename=filename, figsize=figsize)
         # Show plot
         fig.tight_layout()
         if show:
             plt.show()
-        # Save plot if instructed to do so
-        if directory is not None:
-            if filename is None:
-                filename = title.strip('\n') + '.png'
-            save_fig(fig, directory, filename)       
-        return fig     
+        return fig
 
 
 # --------------------------------------------------------------------------- #
@@ -121,119 +84,105 @@ def proper(s):
 # --------------------------------------------------------------------------- #
 #                             Basic Plots                                     #
 # --------------------------------------------------------------------------- #
+
+
 class BasicPlots:
-    """Standard interfaces for basic plotting methods""" 
+    """Standard interfaces for basic plotting methods"""
+
     def __init__(self):
-        pass  
+        pass
 
-    def distplot(self, ax,  data, x, y, z=None, title=None,
-                 log=False, xlim=None, ylim=None):
+    def distplot(self, data, x, y, z=None, title=None,
+                 log=False, xlim=None, ylim=None, directory=None,
+                 filename=None, figsize=(12, 4)):
+        """Renders a univariate distribution of observations."""
 
-        # Initialize figure and settings        
-        sns.set(style="whitegrid", font_scale=1)        
-
-        # Call seaborn method
+        # Initialize axis aesthetics, labels, title, scale and limits
+        fig, ax = self._init_image(x, y, figsize, xlim, ylim, log)
+        # Call seaborn method for plot
         ax = sns.distplot(a=data[x])
-        # Set aesthetics
-        ax.set_facecolor('w')
-        ax.tick_params(colors='k')
-        ax.xaxis.label.set_color('k')
-        ax.yaxis.label.set_color('k')
-        ax.set_title(title, color='k')
-        # Set labels
-        ax.set_xlabel(proper(x))
-        ax.set_ylabel(proper(y))
-        if z is not None:
-            l = ax.legend()
-            l.texts[0].set_text(proper(z))
+        # Save image if path information is provided
+        self._save_plot(fig=fig, directory=directory,
+                        filename=filename, title=title)
+        return fig, ax
 
-        # Change to log scale and impose axis limits if requested
-        if log: ax.set_xscale('log')
-        if ylim is not None: ax.set_ylim(ylim)
-        if xlim is not None: ax.set_xlim(xlim)            
-        return(ax)             
-    
-    def scatterplot(self, ax,  data, x, y, z=None, title=None,
-                 log=False, xlim=None,  ylim=None):
+    def scatterplot(self, data, x, y, z=None, title=None,
+                    log=False, xlim=None, ylim=None, directory=None,
+                    filename=None, figsize=(12, 4)):
+        """Renders and optionally saves a scatterplot."""
 
-        # Initialize figure and settings        
-        sns.set(style="whitegrid", font_scale=1)        
-
-        # # Call seaborn method
+        # Initialize axis aesthetics, labels, title, scale and limits
+        fig, ax = self._init_image(x, y, figsize, xlim, ylim, log)
+        # Call seaborn method for plot
         ax = sns.scatterplot(x=x, y=y, hue=z, data=data, ax=ax, legend='full')
-        # Set aesthetics
-        ax.set_facecolor('w')
-        ax.tick_params(colors='k')
-        ax.xaxis.label.set_color('k')
-        ax.yaxis.label.set_color('k')
-        ax.set_title(title, color='k')
-        # Set labels
-        ax.set_xlabel(proper(x))
-        ax.set_ylabel(proper(y))
-        if z is not None:
-            l = ax.legend()
-            l.texts[0].set_text(proper(z))
-        # Change to log scale and impose axis limits if requested
-        if log: ax.set_xscale('log')
-        if ylim is not None: ax.set_ylim(ylim)
-        if xlim is not None: ax.set_xlim(xlim)            
-        return(ax)         
+        # Save image if path information is provided
+        self._save_plot(fig=fig, directory=directory,
+                        filename=filename, title=title)
+        return fig, ax
 
-    def barplot(self, ax,  data, x, y, z=None, title=None,
-                 log=False, xlim=None, ylim=None):
+    def barplot(self, data, x, y, z=None, title=None,
+                log=False, xlim=None, ylim=None, directory=None,
+                filename=None, figsize=(12, 4)):
+        """Renders and optionally saves a barplot."""
 
-        # Initialize figure and settings        
-        sns.set(style="whitegrid", font_scale=1)
-
-        # # Call seaborn method 
+        # Initialize axis aesthetics, labels, title, scale and limits
+        fig, ax = self._init_image(x, y, figsize, xlim, ylim, log)
+        # Call seaborn method for plot
         ax = sns.barplot(x=x, y=y, hue=z, data=data, ax=ax)
-        # Set aesthetics
-        ax.set_facecolor('w')
-        ax.tick_params(colors='k')
-        ax.xaxis.label.set_color('k')
-        ax.yaxis.label.set_color('k')
-        ax.set_title(title, color='k')
-        # Set labels
-        ax.set_xlabel(proper(x))
-        ax.set_ylabel(proper(y))
-        # Change to log scale and impose axis limits if requested
-        if log: ax.set_xscale('log')
-        if ylim is not None: ax.set_ylim(ylim)
-        if xlim is not None: ax.set_xlim(xlim)            
-        return(ax)     
+        # Save image if path information is provided
+        self._save_plot(fig=fig, directory=directory,
+                        filename=filename, title=title)
+        return fig, ax
 
-    def boxplot(self, ax,  data, x, y, z=None, title=None,
-                 log=False, xlim=None, ylim=None):
+    def boxplot(self, data, x, y, z=None, title=None,
+                log=False, xlim=None, ylim=None, directory=None,
+                filename=None, figsize=(12, 4)):
+        """Renders and optionally saves a boxplot."""
 
-        # Initialize figure and settings        
-        sns.set(style="whitegrid", font_scale=1)  
-
-        # # Call seaborn method 
+        # Initialize axis aesthetics, labels, title, scale and limits
+        fig, ax = self._init_image(x, y, figsize, xlim, ylim, log)
+        # Call seaborn method for plot
         ax = sns.boxplot(x=x, y=y, hue=z, data=data, ax=ax)
-        # Set aesthetics
-        ax.set_facecolor('w')
-        ax.tick_params(colors='k')
-        ax.xaxis.label.set_color('k')
-        ax.yaxis.label.set_color('k')
-        ax.set_title(title, color='k')
-        # Set labels
-        ax.set_xlabel(proper(x))
-        ax.set_ylabel(proper(y))
-        # Change to log scale and impose axis limits if requested
-        if log: ax.set_xscale('log')
-        if ylim is not None: ax.set_ylim(ylim)
-        if xlim is not None: ax.set_xlim(xlim)            
-        return(ax) 
+        # Save image if path information is provided
+        self._save_plot(fig=fig, directory=directory,
+                        filename=filename, title=title)
+        return fig, ax
 
-    def lineplot(self, ax, data, x, y, z=None, title=None,
-                 log=False, xlim=None, ylim=None):
+    def lineplot(self, data, x, y, z=None, title=None,
+                 log=False, xlim=None, ylim=None, directory=None,
+                 filename=None, figsize=(12, 4)):
+        """Renders and optionally saves a lineplot."""
 
-        # Initialize figure and settings        
-        sns.set(style="whitegrid", font_scale=1)
-
-        # # Call seaborn method 
+        # Initialize axis aesthetics, labels, title, scale and limits
+        fig, ax = self._init_image(x, y, figsize, xlim, ylim, log)
+        # Call seaborn method for plot
         ax = sns.lineplot(x=x, y=y, hue=z, data=data, legend='full', ax=ax)
+        # Save image if path information is provided
+        self._save_plot(fig=fig, directory=directory,
+                        filename=filename, title=title)
+        return fig, ax
+
+    def catplot(self, data, x, y, z=None, title=None,
+                log=False, xlim=None, ylim=None, directory=None,
+                filename=None, figsize=(12, 4)):
+        """Renders and optionally saves a catplot."""
+
+        # Initialize axis aesthetics, labels, title, scale and limits
+        fig, ax = self._init_image(x, y, figsize, xlim, ylim, log)
+        # Call seaborn method for plot
+        ax = sns.catplot(x=x, y=y, hue=z, kind='bar', data=data, ax=ax)
+        # Save image if path information is provided
+        self._save_plot(fig=fig, directory=directory,
+                        filename=filename, title=title)
+        return fig, ax
+
+    def _init_image(self, x, y, figsize=(12, 4), xlim=None, ylim=None, title=None, log=False):
+        """Creates and sets the axis aesthetics, labels, scale, and limits."""
+
+        # Initialize plot
+        fig, ax = plt.subplots(figsize=figsize)
         # Set aesthetics
+        sns.set(style="whitegrid", font_scale=1)
         ax.set_facecolor('w')
         ax.tick_params(colors='k')
         ax.xaxis.label.set_color('k')
@@ -242,28 +191,25 @@ class BasicPlots:
         # Set labels
         ax.set_xlabel(proper(x))
         ax.set_ylabel(proper(y))
-        if z is not None:
-            l = ax.legend()
-            l.texts[0].set_text(proper(z))
-        # Change to log scale and impose axis limits if requested
-        if log: ax.set_xscale('log')
-        if ylim is not None: ax.set_ylim(ylim)
-        if xlim is not None: ax.set_xlim(xlim)            
-        return(ax)     
+        # Change to log scale if requested
+        if log:
+            ax.set_xscale('log')
+        # Set x and y axis limits
+        if xlim:
+            if len(xlim) == 1:
+                ax.set_xlim(xlim)
+            else:
+                ax.set_xlim(left=xlim[0], right=xlim[1])
+        if ylim:
+            if len(ylim) == 1:
+                ax.set_ylim(ylim)
+            else:
+                ax.set_xlim(bottom=ylim[0], top=ylim[1])
+        return fig, ax
 
-    def catplot(self, ax,  data, x, y, z=None, title=None,
-                 log=False, xlim=None, ylim=None):
-
-        # Initialize figure and settings        
-        sns.set(style="whitegrid", font_scale=1)
-
-        # Call seaborn method 
-        ax = sns.catplot(x=x, y=y, hue=z, kind='bar', data=data, ax=ax)
-        # Set labels
-        ax.set_xlabels(proper(x))
-        ax.set_ylabels(proper(y))
-        # Change to log scale and impose axis limits if requested
-        if log: ax.set_xscale('log')
-        if ylim is not None: ax.set_ylim(ylim)
-        if xlim is not None: ax.set_xlim(xlim)            
-        return(ax)           
+    def _save_plot(self, fig, directory=None, filename=None, title=None):
+        """Save plot with title to designated directory and filename."""
+        if directory is not None:
+            if filename is None:
+                filename = title.strip('\n') + '.png'
+            save_fig(fig, directory, filename)
