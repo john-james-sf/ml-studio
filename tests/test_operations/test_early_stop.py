a0c552bce8d40cf0da37b8e8fb135f95c6dd8a85
John James <jjames@decisionscients.com>
2019-09-22 22:15:07 -0400

    early stop unit tests, added early stop by strips

--- tests/test_operations/test_early_stop.py
+++ tests/test_operations/test_early_stop.py
@@ -1,17 +1,22 @@
-# --------------------------------------------------------------------------- #
+# =========================================================================== #
 #                            TEST EARLY STOP                                  #
-# --------------------------------------------------------------------------- #
+# =========================================================================== #
 #%%
 import math
 import numpy as np
+import pandas as pd
 import pytest
 from pytest import mark
 
 from ml_studio.operations.early_stop import EarlyStopPlateau
 from ml_studio.operations.early_stop import EarlyStopGeneralizationLoss
 from ml_studio.operations.early_stop import EarlyStopProgress
+from ml_studio.operations.early_stop import EarlyStopStrips
 from ml_studio.operations.metrics import Scorer
 from ml_studio.supervised_learning.regression import LinearRegression
+# --------------------------------------------------------------------------- #
+#                        TEST EARLY STOP PLATEAU                              #
+# --------------------------------------------------------------------------- #
 
 class EarlyStopPlateauTests:
 
@@ -149,3 +154,137 @@ class EarlyStopPlateauTests:
         for i in range(len(logs)):
             stop.on_epoch_end(epoch=i+1, logs=logs[i])
             assert stop.converged == converged[i], "not converging correctly"                      
+
+# --------------------------------------------------------------------------- #
+#                  TEST EARLY STOP GENERALIZATION LOSS                        #
+# --------------------------------------------------------------------------- #
+
+class EarlyStopGeneralizationLossTests:
+
+    @mark.early_stop
+    @mark.early_stop_generalization_loss
+    def test_early_stop_generalization_loss_init(self):    
+        stop = EarlyStopGeneralizationLoss()
+        assert stop.val_size == 0.2, "val_size not set correctly"
+        assert stop.threshold == 2, "threshold not set correctly"
+        assert stop.best_val_cost == np.Inf, "best_val_cost not set correctly"
+
+    @mark.early_stop
+    @mark.early_stop_generalization_loss
+    def test_early_stop_generalization_loss_validation(self):    
+        with pytest.raises(TypeError):
+            stop = EarlyStopGeneralizationLoss(val_size='x')
+            stop.on_train_begin()
+        with pytest.raises(ValueError):
+            stop = EarlyStopGeneralizationLoss(val_size=0)
+            stop.on_train_begin()
+
+    @mark.early_stop
+    @mark.early_stop_generalization_loss
+    def test_early_stop_generalization_loss_on_epoch_end(self):
+        stop = EarlyStopGeneralizationLoss()
+        logs = [{'val_cost': 100,'theta': np.random.rand(4)}, 
+                {'val_cost': 101,'theta': np.random.rand(4)},
+                {'val_cost': 120,'theta': np.random.rand(4)}]
+        converged = [False,False, True]
+        for i in range(len(logs)):
+            stop.on_epoch_end(epoch=i+1, logs=logs[i])
+            assert stop.converged == converged[i], "not converging correctly"                                  
+        assert isinstance(stop.best_weights, (np.ndarray, np.generic)), "best_weights not np.array"
+
+# --------------------------------------------------------------------------- #
+#                         TEST EARLY STOP PROGRESS                            #
+# --------------------------------------------------------------------------- #
+
+class EarlyStopProgressTests:
+
+    @mark.early_stop
+    @mark.early_stop_progress
+    def test_early_stop_progress_init(self):    
+        stop = EarlyStopProgress(threshold=0.25)
+        assert stop.val_size == 0.2, "val_size not set correctly"
+        assert stop.threshold == 0.25, "threshold not set correctly"
+        assert stop.best_val_cost == np.Inf, "best_val_cost not set correctly"
+
+    @mark.early_stop
+    @mark.early_stop_progress
+    def test_early_stop_progress_validation(self):    
+        with pytest.raises(TypeError):
+            stop = EarlyStopProgress(val_size='x')
+            stop.on_train_begin()
+        with pytest.raises(TypeError):
+            stop = EarlyStopProgress(threshold='x')
+            stop.on_train_begin()
+        with pytest.raises(TypeError):
+            stop = EarlyStopProgress(strip_size='x')
+            stop.on_train_begin()            
+
+    @mark.early_stop
+    @mark.early_stop_progress
+    def test_early_stop_progress_on_epoch_end(self):
+        # Obtain train and validation costs
+        filename = "tests/test_operations/test_early_stop.xlsx"
+        df = pd.read_excel(io=filename, sheet_name='progress_data')
+        train_costs = df['train_cost']
+        val_costs = df['val_cost']
+        logs = []
+        for i in range(len(train_costs)):
+            log = {'train_cost': train_costs[i], 'val_cost': val_costs[i]}
+            logs.append(log)
+        # Instantiate and test early stop 
+        stop = EarlyStopProgress()
+        stop.on_train_begin()
+        for i in range(len(train_costs)):
+            stop.on_epoch_end(epoch=i+1, logs=logs[i])
+            if i < len(train_costs)-1:
+                assert stop.converged == False, "not converging at the appropriate time"
+            else:
+                assert stop.converged == True, "not converging at the appropriate time"    
+
+# --------------------------------------------------------------------------- #
+#                         TEST EARLY STOP STRIPS                              #
+# --------------------------------------------------------------------------- #
+
+class EarlyStopStripsTests:
+
+    @mark.early_stop
+    @mark.early_stop_strips
+    def test_early_stop_strips_init(self):    
+        stop = EarlyStopStrips(patience=3)
+        assert stop.val_size == 0.2, "val_size not set correctly"
+        assert stop.strip_size == 5, "strip size not set correctly"
+        assert stop.patience == 3, "patience not set correctly"
+
+    @mark.early_stop
+    @mark.early_stop_strips
+    def test_early_stop_strips_validation(self):    
+        with pytest.raises(TypeError):
+            stop = EarlyStopStrips(val_size='x')
+            stop.on_train_begin()
+        with pytest.raises(TypeError):
+            stop = EarlyStopStrips(patience='x')
+            stop.on_train_begin()
+        with pytest.raises(TypeError):
+            stop = EarlyStopStrips(strip_size='x')
+            stop.on_train_begin()            
+
+    @mark.early_stop
+    @mark.early_stop_strips
+    def test_early_stop_strips_on_epoch_end(self):
+        # Obtain train and validation costs
+        filename = "tests/test_operations/test_early_stop.xlsx"
+        df = pd.read_excel(io=filename, sheet_name='strips_data')
+        val_costs = df['val_cost']
+        logs = []
+        for i in range(len(val_costs)):
+            log = {'val_cost': val_costs[i]}
+            logs.append(log)
+        # Instantiate and test early stop 
+        stop = EarlyStopStrips(patience=3)
+        stop.on_train_begin()
+        for i in range(len(val_costs)):
+            stop.on_epoch_end(epoch=i+1, logs=logs[i])
+            if i < len(val_costs)-1:
+                assert stop.converged == False, "not converging at the appropriate time"
+            else:
+                assert stop.converged == True, "not converging at the appropriate time"                
