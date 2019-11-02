7e42148efd23a0da2e619538fd080d0b488eb1c6
John James <jjames@decisionscients.com>
2019-11-02 04:15:41 -0400

    Added train_test_split

--- ml_studio/utils/data_manager.py
+++ ml_studio/utils/data_manager.py
@@ -10,7 +10,7 @@ import pandas as pd
 from sklearn.base import TransformerMixin, BaseEstimator
 
 # --------------------------------------------------------------------------- #
-#                               Transformers                                  #
+#                               TRANSFORMERS                                  #
 # --------------------------------------------------------------------------- #
 class StandardScaler(BaseEstimator, TransformerMixin):
     def __init__(self, with_mean=True, with_std=True):
@@ -33,7 +33,26 @@ class StandardScaler(BaseEstimator, TransformerMixin):
         X = X * self.s
         X = X + self.mu
         return X
+# --------------------------------------------------------------------------- #
+#                               FUNCTIONS                                     #
+# --------------------------------------------------------------------------- #
+def shuffle_data(X, y, seed=None):
+    """ Random shuffle of the samples in X and y """
+    if seed:
+        np.random.seed(seed)
+    idx = np.arange(X.shape[0])
+    np.random.shuffle(idx)
+    return X[idx], y[idx]
 
+def train_test_split(X, y, test_size=0.3, shuffle=True, seed=None):
+    """ Split the data into train and test sets """
+    if shuffle:
+        X, y = shuffle_data(X, y, seed)
+    split_i = len(y) - int(len(y) // (1 / test_size))
+    X_train, X_test = X[:split_i], X[split_i:]
+    y_train, y_test = y[:split_i], y[split_i:]
+
+    return X_train, X_test, y_train, y_test
 
 def batch_iterator(X, y=None, batch_size=None):
     """ Batch generator """
