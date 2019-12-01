8195a6f70e0bf05fca198e5f74c32c7f0ec6fd42
John James <jjames@decisionscients.com>
2019-11-30 21:16:46 -0500

    rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,7 @@ sys.setrecursionlimit(1500)
 
 import ml_studio
 
+
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -36,7 +37,7 @@ import ml_studio
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
               'sphinx.ext.doctest', 'sphinx.ext.napoleon',
-              'sphinx.ext.intersphinx',
+              'sphinx.ext.intersphinx', 
               'sphinx.ext.imgconverter',
               'sphinx.ext.imgmath'
     ]
@@ -99,7 +100,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -173,9 +174,4 @@ texinfo_documents = [
      'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
-
-
 # %%
